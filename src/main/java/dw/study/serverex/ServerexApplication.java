cbc59104c330f04c5bafb0af0f8f2e1fb82a214d
dowon <devdw98@gmail.com>
2020-09-13 17:30:52 +0900

    initial

---
+++ src/main/java/dw/study/serverex/ServerexApplication.java
@@ -0,0 +1,13 @@
+package dw.study.serverex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dw/study/serverex/ServerexApplicationTests.java
@@ -0,0 +1,13 @@
+package dw.study.serverex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
