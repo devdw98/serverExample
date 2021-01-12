c0f64f0913e1cb50dfe655bfe0ea1eb8464fb526
dowon <devdw98@gmail.com>
2021-01-12 12:03:01 +0900

    test

--- src/main/java/com/wtw/catfriendsServer/domain/FileInfo.java
+++ src/main/java/com/wtw/catfriendsServer/domain/FileInfo.java
@@ -1,4 +1,16 @@
 package com.wtw.catfriendsServer.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class FileInfo {
+    @Column(nullable = false)
+    private String originFilename;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private String filePath;
 }
